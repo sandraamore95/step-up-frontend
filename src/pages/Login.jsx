97380b15a5f6c6bdbd49fae1628ff02eed03eb91
sandra <nasdrahlovejinx@hotmail.com>
2024-04-30 13:11:32 +0200

    styling login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,69 @@
 import { useState } from "react"
+import '../styles/Login.css'
 //conectamos el frontend con el backend -> axios
 import axios from 'axios'
-import {toast} from'react-hot-toast'
-import {useNavigate} from 'react-router-dom'
+import { toast } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
-  
-  const navigate=useNavigate();
-  
-  const [data ,setData]=useState({
-    email:'',
-    password:''
-   })
+
+  const navigate = useNavigate();
+
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  })
 
 
 
-  const loginUser = async(e)=>{
+  const loginUser = async (e) => {
     e.preventDefault()
-    const {email,password}=data
+    const { email, password } = data
     try {
-      const {data}=await axios.post('/login',{
-          email, password
+      const { data } = await axios.post('/login', {
+        email, password
       })   //le pasamos el user (email,password) al endpoint de login
-     if(data.error){
-      toast.error(data.error)
-     }else{
-      setData({})
-      navigate('/dashboard')
-     }
+      if (data.error) {
+        toast.error(data.error)
+      } else {
+        setData({})
+        navigate('/dashboard')
+      }
     } catch (error) {
       console.log(error)
     }
-    
+
   }
-  
+
 
 
   return (
-    <div>
-    <h2>LOGIN</h2>
-    <form onSubmit={loginUser}>
-      <label  htmlFor="">Email</label>
-      <input type="email" placeholder='enter email' value={data.email} onChange={(e)=>setData({...data,email:e.target.value})} />
-      <label htmlFor="">Password</label>
-      <input type="password" placeholder='enter password' value={data.password} onChange={(e)=>setData({...data,password:e.target.value})} />
-      <button type="submit">Login</button>
-    </form>
-  </div>
+    <div className="container login-container">
+      <div className="row">
+        <div className="col-md-6 login-image-container">
+          {/* Imagen de fondo de unas zapatillas */}
+          <img src="https://www.mumka.es/cdn/shop/files/estiloverticalweb_12.png?v=1694191013&width=600" className="img-fluid" alt="Shoes" />
+        </div>
+        <div className="col-md-6 login-form-container">
+          {/* Formulario de inicio de sesión */}
+          <h2 className="text-center mb-4">LOGIN</h2>
+          <form onSubmit={loginUser}>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+
+              <input type="email" className="form-control" placeholder='enter email' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" className="form-control" placeholder='enter password' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+
+            </div>
+            <button type="submit" className="btn btn-primary btn-block">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
+
+
