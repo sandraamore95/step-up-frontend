9babb372a3f37969bcf63b99ba6956b22a86fecb
sandra <nasdrahlovejinx@hotmail.com>
2024-05-28 16:46:57 +0200

    optimizing with api getCollectionByname to handle collectionChange

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -16,8 +16,9 @@ export default function Collection() {
         //Recoge todas las colecciones
         if (response.data.length > 0) {
           // Inicializa  a la primera colección
-          setSelectedCollection(response.data[0].name);
-          setSelectedCollectionData(response.data[0]);
+          const firstCollection = response.data[0].name;
+          setSelectedCollection(firstCollection);
+          fetchCollectionData(firstCollection);
         }
       } catch (error) {
         console.error('Error al obtener las colecciones:', error);
@@ -27,59 +28,70 @@ export default function Collection() {
     fetchCollection();
   }, []);
 
+
+  const fetchCollectionData = async (collectionName) => {
+    try {
+      const response = await axios.get(`/collections/name/${collectionName}`);
+      console.log(response.data);
+      setSelectedCollectionData(response.data);
+    } catch (error) {
+      console.error('Error al obtener los datos de la colección:', error);
+    }
+  };
+
+
   const handleCollectionChange = (event) => {
     const collectionName = event.target.value;
     setSelectedCollection(collectionName);
-    const collection = collections.find(col => col.name === collectionName);
-    setSelectedCollectionData(collection);
+    fetchCollectionData(collectionName);
   };
 
   return (
     <div className="container mt-4">
-      <h2 className="text-center">Colecciones</h2>
-      <div className="form-group">
-        <label htmlFor="collectionSelect" className="form-label-custom">Selecciona una colección:</label>
-        <select
-          id="collectionSelect"
-          className="form-select form-select-custom"
-          value={selectedCollection}
-          onChange={handleCollectionChange}
-        >
-          {collections.map((collection, index) => (
-            <option key={index} value={collection.name}>
-              {collection.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      {selectedCollectionData && (
-        <div className="row mt-4">
-          <div className="col-lg-12">
-            <div className="card shadow-sm">
-              <div className="card-body">
-                <h5 className="card-title">{selectedCollectionData.name}</h5>
-                <p className="card-text">{selectedCollectionData.description}</p>
-                <div className="row">
-                  {selectedCollectionData.includedShoes.map((shoe, index) => (
-                    <div key={index} className="col-md-4 col-sm-6 mb-4">
-                      <div className="card h-100">
-                        <Link to={`/detailShoe/${shoe._id}`}>
-                          <img src={shoe.images[0]} className="card-img-top" alt={`Imagen de ${shoe.brand}`} />
-                        </Link>
-                        <div className="card-body">
-                          <h6 className="card-title">{shoe.brand}</h6>
-                          <p className="card-text">{shoe.model}</p>
-                        </div>
+    <h2 className="text-center">Colecciones</h2>
+    <div className="form-group">
+      <label htmlFor="collectionSelect" className="form-label-custom">Selecciona una colección:</label>
+      <select
+        id="collectionSelect"
+        className="form-select form-select-custom"
+        value={selectedCollection}
+        onChange={handleCollectionChange}
+      >
+        {collections.map((collection, index) => (
+          <option key={index} value={collection.name}>
+            {collection.name}
+          </option>
+        ))}
+      </select>
+    </div>
+    {selectedCollectionData && (
+      <div className="row mt-4">
+        <div className="col-lg-12">
+          <div className="card shadow-sm">
+            <div className="card-body">
+              <h5 className="card-title">{selectedCollectionData.name}</h5>
+              <p className="card-text">{selectedCollectionData.description}</p>
+              <div className="row">
+                {selectedCollectionData.includedShoes.map((shoe, index) => (
+                  <div key={index} className="col-md-4 col-sm-6 mb-4">
+                    <div className="card h-100">
+                      <Link to={`/detailShoe/${shoe._id}`}>
+                        <img src={shoe.images[0]} className="card-img-top" alt={`Imagen de ${shoe.brand}`} />
+                      </Link>
+                      <div className="card-body">
+                        <h6 className="card-title">{shoe.brand}</h6>
+                        <p className="card-text">{shoe.model}</p>
                       </div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    )}
+  </div>
 
   );
 }
