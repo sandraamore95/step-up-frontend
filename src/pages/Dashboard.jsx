05872bce79aa39564c005937f2151e9176ecd3d9
sandra <nasdrahlovejinx@hotmail.com>
2024-05-28 17:06:58 +0200

    adding to dashboard menu with component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,11 @@
 
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { useContext } from 'react'
+import '../styles/Dashboard.css'
 import { UserContext } from '../context/userContext'
+import Login from './Login';
+import WishList from './WishList';
+import Register from './Register';
 
 export default function Dashboard() {
     // const {user}=useContext(UserContext) 
@@ -9,21 +13,42 @@ export default function Dashboard() {
     const context = useContext(UserContext);
     const user = context.user; // recogemos el objeto user que generamos en el backend del controlador -> profile
   
-    useEffect(() => {
-      console.log(user);
-  }, []);
+    const [selectedOption, setSelectedOption] = useState('edit-info');
 
-
-  return (
-    <div>
-    <h1>Dashboard</h1>
-    <h2>Zapatillas guardadas</h2>
-    {user && ( 
-      <div>
-        <h2>Hi {user.name}</h2>
-        <h3>{user.email}</h3>
+    const handleOptionChange = (option) => {
+      setSelectedOption(option);
+    };
+  
+    const renderSelectedComponent = () => {
+      switch (selectedOption) {
+        case 'edit-info':
+          return <Login />;
+        case 'favorites':
+          return <WishList />;
+        case 'orders':
+          return <Register />;
+        default:
+          return null;
+      }
+    };
+  
+    return (
+      <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="menu-container">
+            <h3>Menú</h3>
+            <ul className="menu-list">
+              <li className={selectedOption === 'edit-info' ? 'active' : ''} onClick={() => handleOptionChange('edit-info')}>Editar Información</li>
+              <li className={selectedOption === 'favorites' ? 'active' : ''} onClick={() => handleOptionChange('favorites')}>Lista de Favoritos</li>
+              <li className={selectedOption === 'orders' ? 'active' : ''} onClick={() => handleOptionChange('orders')}>Ver Pedidos</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-md-9">
+          {renderSelectedComponent()}
+        </div>
       </div>
-    )}
-  </div>
-  )
-}
+    </div>
+    );
+  }
