104f2ce03a3c51903935d9ab60a905918a911420
sandra <nasdrahlovejinx@hotmail.com>
2024-06-14 15:08:20 +0200

    modifying cartContext

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect,useContext } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { UserContext } from './userContext';
 
@@ -13,12 +13,13 @@ const CartProvider = ({ children }) => {
 
   useEffect(() => {
     if (user) {
+      console.log("somos usuario autenticado");
       const fetchCart = async () => {
         try {
           const response = await axios.get(`/cart/cart-user`);
           console.log(response.data.cart.products);
           setCartItems(response.data.cart.products);
-          console.log("el carrito esta asi ",cartItems);
+          console.log("el carrito esta asi ", cartItems);
         } catch (error) {
           console.error('Error fetching cart for authenticated user:', error);
         }
@@ -32,50 +33,60 @@ const CartProvider = ({ children }) => {
     if (!user) {
       console.log("estamos aqui porque somos usuario invitado");
       localStorage.setItem('cart-user', JSON.stringify(cartItems));
-      console.log("el carrito esta asi ",cartItems);
+      console.log("el carrito esta asi ", cartItems);
     } else {
-      
+      //cuando eres usuario invitado con localstorage items y vas a iniciar sesion 
+ console.log("eres usuario autenticado con items en el localstorage + backend cartUser");
 
-  const saveCart = async () => {
+ const savedCart = JSON.parse(localStorage.getItem('cart-user')) || []; console.log(savedCart);
+      const saveCart = async () => {
         try {
           console.log(cartItems);
-          if(cartItems.length!=0){
-            await axios.post(`/cart/add`, { cart: cartItems });
+          if (cartItems.length != 0) {
+          
+            await axios.post(`/cart/add`, { cart: savedCart });
           }
-         
+
         } catch (error) {
           console.error('Error saving cart for authenticated user:', error);
         }
       };
 
-saveCart();
-   
-      // Optionally, save the cart to the backend for authenticated users
-    
-
-      
+      saveCart();
     }
-  }, [cartItems, user]);
+  }, [cartItems, user]); // cuando el usuario inicia sesion  || cuando el localStorage se añade al backend cambia el cartItems , asi que vuelve a empezar el useEffect()
+
 
   const addToCart = (product, quantity, size) => {
-      // Buscar si el producto ya existe en el carrito con la misma talla
-    const existingItem = cartItems.find(
+    // Buscar si el producto ya existe en el carrito con la misma talla
+    const existingItemIndex = cartItems.findIndex(
       (item) => item.product._id === product._id && item.size === size
     );
-    //si el producto ya existe en el carrito con la misma talla, se incrementa la cantidad en lugar de añadir un nuevo elemento.
-    if (existingItem) {
-      setCartItems(
-        cartItems.map((item) =>
-          item.product._id === product._id && item.size === size
-            ? { ...item, quantity: item.quantity + quantity }
-            : item // se devuelve el item actual sin cambios
-        )
-      );
+  
+    // Si el producto ya existe en el carrito con la misma talla, se incrementa la cantidad
+    if (existingItemIndex !== -1) {
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[existingItemIndex].quantity += quantity;
+      setCartItems(updatedCartItems);
     } else {
-      //crear un nuevo arreglo que es una copia de cartItems, con un objeto adicional agregado al final
-      setCartItems([...cartItems, { product, quantity, size }]);
+      // Si no existe, se añade un nuevo elemento al arreglo de productos
+      const updatedCartItems = [
+        ...cartItems,
+        {
+          product: {
+            _id: product._id,
+            // Agregar otras propiedades del producto si es necesario
+          },
+          quantity,
+          size,
+        },
+      ];
+  
+      setCartItems(updatedCartItems);
+      
     }
   };
+  
 
   const updateQuantity = (productId, size, quantity) => {
     setCartItems(
