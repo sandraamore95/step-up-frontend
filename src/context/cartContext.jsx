1de02a674fdba18ee3a4e53fca51c075354dbb7e
sandra <nasdrahlovejinx@hotmail.com>
2024-07-29 20:53:16 +0200

    adding pagination to wishlist component

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -4,6 +4,20 @@ import { UserContext } from './userContext';
 
 export const CartContext = createContext();
 
+//PROBLEMAS PARA SOLUCIONAR
+/*
+1) LOCALSTORAGE AL RECARGAR LA PAGINA , SE VA ALA MIERDA EL CARRITOLOCAL - XXXXXXX
+2) YA FUNCIONA LO DE ELIMINAR DE GOLPE REMOVE PARA LOCALSTORAGE INVITADOS - DONE
+3) DISMINUIR Y AUMENTAR LA CANTIDAD DE LA ZAPATILLA EN LOCALSTORAGE INVITADOS - DONE
+4) parte BACKEND - MIRAR ELIMINAR CANTIDAD , O REMOVE TOTAL , O SUMAR CANTIDAD TAMTBIEN  -XXXXXXX
+
+
+
+
+*/
+
+
+
 const CartProvider = ({ children }) => {
   const { user } = useContext(UserContext);
   const [cartItems, setCartItems] = useState(() => {
@@ -28,7 +42,7 @@ const CartProvider = ({ children }) => {
     }
   }, [user]);
 
-  
+
   // USE EFFECT USUARIO INVITADO -> LOCALSTORAGE
   useEffect(() => {
     if (!user) {
@@ -36,38 +50,38 @@ const CartProvider = ({ children }) => {
       localStorage.setItem('cart-user', JSON.stringify(cartItems));
       console.log("el carrito esta asi ", cartItems);
     }
-    }, [cartItems]);
+  }, [cartItems]);
 
 
-    //USE EFFECT SINCRONIZACION CARRITO LOCALSTORAGE + BACKEND
-    useEffect(() => {
-      if (user) {
-        const syncCartLocalWithBackend = async () => {
-          try {
-            const savedCart = JSON.parse(localStorage.getItem('cart-user')) || [];
-            if (savedCart.length > 0) {
-              console.log("sincronizando carrito local con backend");
-              await axios.post(`/cart/add`, { cart: savedCart });
-              localStorage.removeItem('cart-user');
-            }
-            // Fetch the updated cart from backend to ensure cartItems is up-to-date
-            const response = await axios.get(`/cart/cart-user`);
-            setCartItems(response.data.cart.products);
-          } catch (error) {
-            console.error('Error syncing local cart with backend:', error);
+  //USE EFFECT SINCRONIZACION CARRITO LOCALSTORAGE + BACKEND
+  useEffect(() => {
+    if (user) {
+      const syncCartLocalWithBackend = async () => {
+        try {
+          const savedCart = JSON.parse(localStorage.getItem('cart-user')) || [];
+          if (savedCart.length > 0) {
+            console.log("sincronizando carrito local con backend");
+            await axios.post(`/cart/add`, { cart: savedCart });
+            localStorage.removeItem('cart-user');
           }
-        };
-        syncCartLocalWithBackend();
-      }
-    }, [user]);
+          // Fetch the updated cart from backend to ensure cartItems is up-to-date
+          const response = await axios.get(`/cart/cart-user`);
+          setCartItems(response.data.cart.products);
+        } catch (error) {
+          console.error('Error syncing local cart with backend:', error);
+        }
+      };
+      syncCartLocalWithBackend();
+    }
+  }, [user]);
 
 
   //USE EFFFECT CUANDO EL USUARIO CIERRA SESION - DELETE LOCALSTORAGE CARRITO
   useEffect(() => {
-      if (!user) {
-        localStorage.removeItem('cart-user');
-        setCartItems([])
-      }
+    if (!user) {
+      localStorage.removeItem('cart-user');
+      setCartItems([])
+    }
   }, [user]);
 
 
@@ -80,7 +94,7 @@ const CartProvider = ({ children }) => {
         const response = await axios.post(`/cart/add`, { cart: [newItem] });
         const updatedCart = response.data.cart.products;
         setCartItems(updatedCart);
-      
+
       } else {
         const productDetails = await axios.get(`/shoes/${newItem.product}`);
         console.log(productDetails.data);
@@ -88,13 +102,13 @@ const CartProvider = ({ children }) => {
           ...newItem,
           product: productDetails.data
         };
-  
+
         // Añadir el producto a local storage para usuarios invitados
         setCartItems(prevItems => {
           const existingItemIndex = prevItems.findIndex(
             item => item.product._id === fullNewItem.product._id && item.size === fullNewItem.size
           );
-  
+
           let updatedCart;
           if (existingItemIndex >= 0) {
             updatedCart = prevItems.map((item, index) =>
@@ -105,7 +119,7 @@ const CartProvider = ({ children }) => {
           } else {
             updatedCart = [...prevItems, fullNewItem];
           }
-  
+
           localStorage.setItem('cart-user', JSON.stringify(updatedCart));
           return updatedCart;
         });
@@ -114,8 +128,8 @@ const CartProvider = ({ children }) => {
       console.error('Error adding item to cart:', error);
     }
   };
-  
-  
+
+
   const updateQuantity = (productId, size, quantity) => {
     setCartItems(
       cartItems.map((item) =>
@@ -136,25 +150,43 @@ const CartProvider = ({ children }) => {
     );
   };
 
-  
+
 
   const removeFromCart = async (item) => {
-    const productId=item.product._id;
-    const size=item.size;
-    console.log(item);
-    try {
-      const response = await axios.put('/cart/delete', {
-        data: {
-          product:item
-        }
-      });
-      console.log(response.data.message);
-  
-      // Update the cart items in the frontend
-      setCartItems(cartItems.filter((item) => !(item.product._id === productId && item.size === size)));
-    } catch (error) {
-      console.error('Error removing item from cart:', error);
+    console.log('el producto ha borrar', item);
+    const productId = item.product._id;
+    const size = item.size;
+    // primero tenemos que ver si es invitado o usuario
+    if (!user) {
+      console.log("es invitado");
+      //recogemos el localCarrito
+      let cart = JSON.parse(localStorage.getItem('cart-user')) || [];
+      console.log(cart);
+      // Filtrar el carrito para eliminar el producto
+      cart = cart.filter(cartItem => !(cartItem.product._id === productId && cartItem.size === size));
+      console.log(cart);
+      // Guardar el carrito actualizado en el localStorage
+      localStorage.setItem('cart-user', JSON.stringify(cart));
+      // Actualizar el estado del carrito en el frontend
+      setCartItems(cart);
+    } else {
+      try {
+        const response = await axios.put('/cart/delete', {
+          data: {
+            product: item
+          }
+        });
+        console.log(response.data.message);
+
+        // Update the cart items in the frontend
+        setCartItems(cartItems.filter((item) => !(item.product._id === productId && item.size === size)));
+      } catch (error) {
+        console.error('Error removing item from cart:', error);
+      }
     }
+
+
+
   };
 
   return (
--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import '../styles/WishListStyle.css'
 
 export default function WishList() {
-    const [favoriteShoes, setFavoriteShoes] = useState([]);
+  const [favoriteShoes, setFavoriteShoes] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const shoesPerPage = 6;
 
   useEffect(() => {
     const fetchFavoriteShoes = async () => {
       try {
         const response = await axios.get(`/favorites`);
-        setFavoriteShoes(response.data.favoriteShoes.shoes); 
+        setFavoriteShoes(response.data.favoriteShoes.shoes);
       } catch (error) {
         console.error('Error al obtener las zapatillas favoritas del usuario:', error);
       }
@@ -22,26 +25,60 @@ export default function WishList() {
   if (favoriteShoes.length === 0) {
     return <div>No hay zapatillas favoritas.</div>;
   }
+  // Calcular los índices de los zapatos actuales
+  const indexOfLastShoe = currentPage * shoesPerPage;
+  const indexOfFirstShoe = indexOfLastShoe - shoesPerPage;
+  const currentShoes = favoriteShoes.slice(indexOfFirstShoe, indexOfLastShoe);
 
- 
-  return (
-<div className="container mt-4">
-  <h3>Zapatillas Favoritas</h3>
-  <ul>
-  {favoriteShoes.map((shoe, index) => (
-                    <div key={index} className="col-md-4 mb-4">
-                        <div className="card">
-                            <Link to={`/detailShoe/${shoe._id}`}>
-                                <img src={shoe.images[0]} className="card-img-top" alt={`Imagen de ${shoe.brand}`} />
-                            </Link>
-                            <div className="card-body">
-                                <h5 className="card-title">{shoe.brand} - {shoe.model}</h5>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-  </ul>
-</div>
-  
+   // Cambiar de página
+   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+   return (
+    <div className="container mt-4">
+      <h3>Zapatillas Favoritas</h3>
+      <div className="row">
+        {currentShoes.map((shoe, index) => (
+          <div key={index} className="col-md-4 mb-4">
+            <div className="card h-100">
+              <Link to={`/detailShoe/${shoe._id}`}>
+                <img src={shoe.images[0]} className="card-img-top" alt={`Imagen de ${shoe.brand}`} />
+              </Link>
+              <div className="card-body">
+                <h5 className="card-title">{shoe.brand} - {shoe.model}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Pagination
+        shoesPerPage={shoesPerPage}
+        totalShoes={favoriteShoes.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
+    </div>
   );
 }
+
+// Componente de Paginación
+const Pagination = ({ shoesPerPage, totalShoes, paginate, currentPage }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalShoes / shoesPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className="pagination justify-content-center">
+        {pageNumbers.map(number => (
+          <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+            <button onClick={() => paginate(number)} className="page-link">
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
